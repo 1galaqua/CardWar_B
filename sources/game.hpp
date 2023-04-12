e0d11651c23223b8d59220f92c27644d7f646d8f
1galaqua <92546767+1galaqua@users.noreply.github.com>
2023-04-12 10:29:05 +0300

    final vers

---
+++ sources/game.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#ifndef GAME_H
+#define GAME_H
+#include <iostream>
+#include "player.hpp"
+#include <vector>
+namespace ariel{
+    
+class Game {
+
+   private:
+    Player &player1;
+    Player &player2;
+    vector<string> log;
+    string winner="draw";
+    vector<Card> deck;
+public:
+
+    
+    Game(Player& player01, Player& player02);
+   
+    vector<Card> mydeck(){return this->deck;}
+
+    void shuffleDeck(vector<Card>& deck);
+
+    void deal_cards();
+    
+    void playTurn();
+
+    void printLastTurn();
+    
+    void playAll(); 
+    
+    void printWiner(){cout<<"the winner is: "+this->winner<<endl;}
+
+    void playTurnafterdraw(int draw_num); 
+
+    void printLog();
+    
+    void printStats();
+
+
+
+    
+        
+
+};
+}
+
+#endif
---
+++ sources/player.cpp
@@ -0,0 +1,46 @@
+#include "player.hpp"
+#include "card.hpp"
+#include "string"
+using namespace std;
+using namespace ariel;
+// #include <algorithm>
+// #include <random>
+// auto rng = std::default_random_engine {};
+//     shuffle(std::begin(this->mycards), std::end(this->mycards), rng);
+Player::Player(){};
+ Player::Player (string name){
+    this->myname=name;
+    
+    
+ }
+
+    
+
+
+
+   int Player::stacksize(){
+       return this->mycards.size();
+   }
+
+   int Player::cardesTaken(){
+        
+       return this->num_won;
+   }
+
+   
+    Card Player::throw_card(){
+        
+        if(!this->mycards.empty()){
+            
+            Card temp=this->mycards.back();
+            
+            this->mycards.pop_back();
+            return temp;
+        }else {
+            
+            throw std::invalid_argument("empty stack");}
+
+        return Card();
+    }
+
+    
